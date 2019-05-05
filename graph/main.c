1fc86c31746eedf7e86a9cc6f761655d562a411f
manturovDan <manturov.dan@mail.ru>
2019-05-05 16:48:02 +0300

    dsa

--- graph/main.c
+++ graph/main.c
@@ -1,4 +1,37 @@
-//
-// Created by danila on 05.05.19.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
+int get_Int(int *a, int nat) {
+    int n;
+    do {
+        n = scanf("%d", a);
+        if (n == 0) {
+            printf("Incorrect input. Try again!\n");
+            scanf("%*f^\n");
+            scanf("%*s");
+            continue;
+        }
+
+        if (*a > 100000000) {
+            printf("Input number <= 100M\n");
+            n = 0;
+        }
+
+        if(nat == 1 && *a < 0) {
+            printf("Input natural number\n");
+            n = 0;
+        }
+
+    } while(n == 0);
+
+    scanf("%*1[\n]");
+    return n < 0 ? 0 : 1;
+}
+
+int get_nInt(int *a) {
+    return get_Int(a, 1);
+}
+
+int main() {
+    printf("Test");
+}
