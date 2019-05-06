bbcf9ba35881c972a0d3f9486f7aeb084c20354a
manturovDan <manturov.dan@mail.ru>
2019-05-06 09:13:38 +0300

    creating Graph

---
+++ graph/graph.c
@@ -0,0 +1,93 @@
+struct Neighbour {
+	int name;
+	float dist;
+	struct Neighbour *next;
+} typedef Neighbour;
+
+struct Node {
+	int name;
+	float x;
+	float y;
+	Neighbour *nbr;
+} typedef Node;
+
+struct CluItem {
+	Node *fNode;
+	struct CluItem *next;
+} typedef CluItem;
+
+struct GraphClutch {
+	int n;
+	CluItem *grTab[SIZE];
+} typedef GraphClutch;
+
+int hash(int name) {
+	return name % SIZE;
+}
+
+CluItem *find(GraphClutch *gTab, int name) {
+	int tabPlace = hash(name);
+
+	CluItem *cont = gTab->grTab[tabPlace];
+	if(cont == NULL) {
+		return NULL;
+	}
+
+	while(cont != NULL) {
+		if(cont->fNode->name == name) {
+			return cont;
+		}
+		cont = cont->next;
+	}
+
+	return NULL;
+}
+
+int adding(GraphClutch *gTab, int name, float x, float y) { //returns: 2 - is already exist
+	if (find(gTab, name) != NULL) {
+		return 2;
+	}
+
+	int tabPlace = hash(name);
+
+	CluItem *newCont = (CluItem *)calloc(1, sizeof(CluItem));
+	if (gTab->grTab[tabPlace] != NULL)
+		newCont->next = gTab->grTab[tabPlace];
+	newCont->fNode = (Node *)calloc(1, sizeof(Node));
+	newCont->fNode->name = name;
+	newCont->fNode->x = x;
+	newCont->fNode->y = y;
+	newCont->fNode->nbr = NULL;
+	gTab->grTab[tabPlace] = newCont;
+	gTab->n++;
+
+	return 0;	
+}
+
+float calcDist(float x1, float y1, float x2, float y2) {
+	return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+}
+
+int edge(GraphClutch *gTab, int from, int to) {
+	CluItem *nfrom = find(gTab, from);
+	CluItem *nto = find(gTab, to);
+	if(nfrom == NULL || nto == NULL) {
+		return 2; //no nodes
+	}
+
+	Neighbour *nbr = nfrom->fNode->nbr;
+	while (nbr != NULL) {
+		if(nbr->name == to) {
+			return 1; //way exists
+		}
+		nbr = nbr->next;
+	} 
+
+	Neighbour *newCon = (Neighbour *)calloc(1, sizeof(Neighbour));
+	if(nfrom->fNode->nbr != NULL) 
+		newCon->next = nfrom->fNode->nbr;
+	newCon->name = nto->fNode->name;
+	newCon->dist = calcDist(nfrom->fNode->x, nfrom->fNode->y, nto->fNode->x, nto->fNode->y);
+	nfrom->fNode->nbr = newCon;
+
+}
--- graph/main.c
+++ graph/main.c
@@ -1,10 +1,15 @@
+#define SIZE 100
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "graph.c"
 
-int get_Int(int *a, int nat) {
+int get_Num(int *a, int nat) { //nat 1 if need natural number (>= 0)
     int n;
     do {
         n = scanf("%d", a);
+
         if (n == 0) {
             printf("Incorrect input. Try again!\n");
             scanf("%*f^\n");
@@ -28,10 +33,182 @@ int get_Int(int *a, int nat) {
     return n < 0 ? 0 : 1;
 }
 
+int get_Int(int *a) {
+	return get_Num(a, 0);
+}
+
 int get_nInt(int *a) {
-    return get_Int(a, 1);
+    return get_Num(a, 1);
+}
+
+int get_Float(float *a, int bez) { // bez 1 if we need >= number
+	int n;
+    do {
+        n = scanf("%f", a);
+
+        if (n == 0) {
+            printf("Incorrect input. Try again!\n");
+            scanf("%*f^\n");
+            scanf("%*s");
+            continue;
+        }
+
+        if (*a > 100000000) {
+            printf("Input number <= 100M\n");
+            n = 0;
+        }
+
+        if(bez == 1 && *a < 0) {
+            printf("Input natural number\n");
+            n = 0;
+        }
+
+    } while(n == 0);
+
+    scanf("%*1[\n]");
+    return n < 0 ? 0 : 1;
+}
+
+int D_Add(GraphClutch *gTab) {
+	int res, name;
+	float x, y;
+	printf("Input name (int) of Graph node\n");
+	res = get_nInt(&name);
+	printf("Input x-coord (float) of new node\n");
+	res *= get_Float(&x, 0);
+	printf("Input y-coord (float) of new node\n");
+	res *= get_Float(&y, 0);
+	if (res == 0) {
+		printf("\nError.Try again\n");
+		return 1;
+	}
+
+	printf("Adding Node\n");
+	return adding(gTab, name, x, y);
+}
+
+int D_Show(GraphClutch *gTab) {
+	int emp = 0;
+	for (int i = 0; i < SIZE; i++) {
+		CluItem *cont = gTab->grTab[i];
+		if(cont != NULL) {
+			printf("(%d)\n", i);
+			while(cont != NULL) {
+				printf("%d (%f; %f)", cont->fNode->name, cont->fNode->x, cont->fNode->y);
+				Neighbour *nbr = cont->fNode->nbr;
+				while (nbr != NULL) {
+					printf(" --%f-->(%d) ", nbr->dist, nbr->name);
+					nbr = nbr->next;
+				} 
+				printf("\n");
+				cont = cont->next;
+			}
+			emp = 1;
+		}
+	}
+
+	if (emp == 0) {
+		printf("Graph is empty\n");
+		return 1;
+	}
+
+	return 0;
+}
+
+int D_Add_Edge(GraphClutch *gTab) {
+	int res, from, to;
+	printf("Input name of starting node\n");
+	res = get_nInt(&from);
+	printf("Input name of ending node\n");
+	res *= get_nInt(&to);
+	if (res == 0) {
+		printf("Error\n");
+		return 1;
+	}
+	int eres = edge(gTab, from, to);
+
+	if (eres == 1) {
+		printf("The way is already exist\n");
+		return 1;
+	}
+	else if (eres == 2) {
+		printf("Error! There aren\'t these nodes in Graph\n");
+		return 1;
+	}
+
+	return 0;
+
 }
 
 int main() {
-    printf("Test");
+	char *msgs[] = {"0. Quit", "1. Add node to Graph", "2. Add edge to Graph", 
+					"3. Delete node from Graph",
+					"4. Find 3 shortest ways between all nodes (Floyd–Warshall algorithm)",
+					"5. Print Graph", "6. Save Graph in file"};
+
+
+	while (1) {
+		printf("This is Graph program. Choose one option\n1. Create Graph by hand\n2. Generate graph\n3. Load Graph from file\n");
+		int mode;
+		if (get_nInt(&mode) == 0) {
+			printf("Error. Try again\n");
+			continue;
+		}
+		else if(mode == 1) {
+			printf("Create your graph\n");
+			break;
+		}
+		else if(mode == 2) {
+			printf("Random graph\n");
+			break;
+		}
+		else if(mode == 3) {
+			printf("Load graph\n");
+			break;
+		}
+		else {
+			printf("Input correct value\n");
+			continue;
+		}
+
+	}
+    
+    GraphClutch gTab;
+    gTab.n = 0;
+    for (int i =0; i < SIZE; i++) {
+    	gTab.grTab[i] = NULL;
+    }
+
+    int mItem;
+    int res;
+    while (1) {
+    	for (int i = 0; i < sizeof(msgs) / sizeof(msgs[0]); i++) {
+    		printf("%s\n", msgs[i]);
+    	}
+
+    	if (get_nInt(&mItem) == 0) {
+    		printf("\nError.Try again\n");
+    		continue;
+    	}
+
+    	if (mItem == 0)
+    		break;
+    	else if (mItem == 1) {
+    		res = D_Add(&gTab);
+    		if (res == 2) {
+    			printf("Element is exist\n");
+    		}
+    		else if(res == 0) {
+    			printf("Successfully added\n");
+    		}
+    	}
+    	else if(mItem == 2) {
+    		D_Add_Edge(&gTab);
+    	}
+    	else if(mItem == 5) {
+    		D_Show(&gTab);
+    	}
+    }
+
+    printf("End of program. Make clearing\n");
 }
